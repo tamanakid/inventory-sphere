e613b41c0879b584246582372b29ac36c41cc967
tamanakid <nfvillanuevap@gmail.com>
2022-05-15 10:54:34 +0200

    Add other role group permission classes

--- api_internal/permissions/user_role_permissions.py
+++ api_internal/permissions/user_role_permissions.py
@@ -9,20 +9,57 @@ from .base_api_permission import BaseAPIPermission
 
 write_methods = ['POST', 'PUT', 'DELETE']
 
+manager_roles = [User.Role.INVENTORY_MANAGER, User.Role.STORAGE_MANAGER]
+storage_roles = [User.Role.INVENTORY_MANAGER, User.Role.STORAGE_MANAGER, User.Role.STORAGE_EMPLOYEE]
+data_roles = [User.Role.INVENTORY_MANAGER, User.Role.DATA_EMPLOYEE]
 
+
+class InventoryManagerPermission(BasePermission):
+    def has_permission(self, request, view):
+        is_inventory_manager = request.user.role == User.Role.INVENTORY_MANAGER
+
+        return is_inventory_manager
+
+
+# For most customization features (i.e. Categorization, Product, etc.)
 class InventoryManagerWriteElseReadOnlyPermission(BasePermission):
     def has_permission(self, request, view):
-        # has_base_perm = super().has_permission(request, view)
         is_write_method = request.method in write_methods
         is_inventory_manager = request.user.role == User.Role.INVENTORY_MANAGER
 
-        # return has_base_perm and (is_inventory_manager or not is_write_method)
         return is_inventory_manager or not is_write_method
-    
-    def has_object_permission(self, request, view, object):
-        # has_base_perm = super().has_permission(request, view)
-        is_write_method = request.method in write_methods
-        is_inventory_manager = request.user.role == User.Role.INVENTORY_MANAGER
 
-        # return has_base_perm and (is_inventory_manager or not is_write_method)
-        return is_inventory_manager or not is_write_method
+
+# i.e. View List of Users
+class ManagerRolesPermission(BasePermission):
+    def has_permission(self, request, view):
+        is_manager = request.user.role in manager_roles
+
+        return is_manager
+
+
+# i.e. Approval of some Storage Operations
+class ManagerRolesWriteElseReadOnlyPermission(BasePermission):
+    def has_permission(self, request, view):
+        is_write_method = request.method in write_methods
+        is_manager = request.user.role in manager_roles
+
+        return is_manager or not is_write_method
+
+
+# All other storage operations
+class StorageRolesWriteElseReadOnlyPermission(BasePermission):
+    def has_permission(self, request, view):
+        is_write_method = request.method in write_methods
+        is_storage_role = request.user.role in storage_roles
+
+        return is_storage_role or not is_write_method
+
+
+# Future Data-related operations (i.e., Reports)
+class DataRolesPermissions(BasePermission):
+    def has_permission(self, request, view):
+        is_write_method = request.method in write_methods
+        is_storage_role = request.user.role in storage_roles
+
+        return is_storage_role or not is_write_method
